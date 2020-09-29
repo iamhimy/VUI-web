75832402e2b4013e8a29229670728514a5db9689
iamhimy <69810814+iamhimy@users.noreply.github.com>
2020-09-29 21:03:26 +0530

    Add files via upload

---
+++ my.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+print("content-type : text/html")
+print()
+
+import cgi
+import subprocess
+
+form=cgi.FieldStorage()
+cmd=form.getvalue("x")
+
+z=subprocess.getstatusoutput(cmd)
+print(z)
+print('hello')
